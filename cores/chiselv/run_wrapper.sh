c793f86eae5e276ca5563309ec52750a6bd34356
Carlos de Paula <me@carlosedp.com>
2022-09-12 11:49:48 -0300

    Use working image for formal

--- cores/chiselv/run_wrapper.sh
+++ cores/chiselv/run_wrapper.sh
@@ -1,1 +1,4 @@
-docker run --rm -it -v "$(realpath ../../)":/src -w /src/cores/"$(basename $(realpath .))" gcr.io/hdl-containers/formal $@
+#!/bin/bash
+IMG=gcr.io/hdl-containers/formal@sha256:ff660de17fb8847f01fb33c1b4bd8bf08eebfd9e3fdaa9dd844a71f7c54b42a7
+
+docker run --rm -it -v "$(realpath ../../)":/src -w /src/cores/"$(basename $(realpath .))" ${IMG} "$@"
--- cores/chiselv/start_formal.sh
+++ cores/chiselv/start_formal.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# rm -rf checks disasm.o disasm.s RVFITop.v
-# make -C ../../.. rvfi
-# cp -R ../../../generated_rvfi/*.v .
+IMG=gcr.io/hdl-containers/formal@sha256:ff660de17fb8847f01fb33c1b4bd8bf08eebfd9e3fdaa9dd844a71f7c54b42a7
+# Newer images have a bug: https://github.com/YosysHQ/sby/issues/216
+
+# Uncomment below to build new core .v
+# make -C ../../../chiselv rvfi
+# cp -R ../../../chiselv/generated_rvfi/*.v .
 rm -rf checks
-docker run --rm -it -v "$(realpath ../../)":/src -w /src/cores/"$(basename $(realpath .))" gcr.io/hdl-containers/formal python3 ../../checks/genchecks.py
-docker run --rm -it -v "$(realpath ../../)":/src -w /src/cores/"$(basename $(realpath .))" gcr.io/hdl-containers/formal make -C checks -j"$(nproc)"
+docker run --rm -it -v "$(realpath ../../)":/src -w /src/cores/"$(basename $(realpath .))" ${IMG}  python3 ../../checks/genchecks.py
+docker run --rm -it -v "$(realpath ../../)":/src -w /src/cores/"$(basename $(realpath .))" ${IMG}  make -C checks -j"$(nproc)"
